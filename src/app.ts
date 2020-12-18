5e1a22fdb907b1b7b9a2d042aa07a9e9127d5bae
poonsht <poon_arsene_lupin@hotmail.com>
2020-12-18 18:26:28 +0700

    [+] add : env.PORT for cloud run

--- src/app.ts
+++ src/app.ts
@@ -3,49 +3,51 @@ import config from './config/config'
 import MongoAdapter from './adapters/mongo.adapter'
 
 class App {
-  public app: FastifyInstance
-  public app_doamain: string = config.app.domain
-  public app_port: number = parseInt(`${config.app.port}`, 10) ?? 8080
+	public app: FastifyInstance
+	public app_doamain: string = config.app.domain
+	public app_port: number = parseInt(`${config.app.port}`, 10) ?? 8080
 
-  private databaseInfo = {
-    username: config.db.mongo.username!,
-    password: config.db.mongo.password!,
-    host: config.db.mongo.host!,
-    port: parseInt(`${config.db.mongo.port}`, 10) ?? 27017,
-    dbName: config.db.mongo.name!,
-    authName: config.db.mongo.auth!,
-  }
+	private databaseInfo = {
+		username: config.db.mongo.username!,
+		password: config.db.mongo.password!,
+		host: config.db.mongo.host!,
+		port: parseInt(`${config.db.mongo.port}`, 10) ?? 27017,
+		dbName: config.db.mongo.name!,
+		authName: config.db.mongo.auth!,
+	}
 
-  constructor(appInit: { plugins: any; routes: any }) {
-    this.app = fastify({ logger: true })
-    this.connectDatabase()
-    this.pluginsRegister(appInit.plugins)
-    this.routes(appInit.routes) 
-  }
+	constructor(appInit: { plugins: any; routes: any }) {
+		this.app = fastify({ logger: true })
+		this.connectDatabase()
+		this.pluginsRegister(appInit.plugins)
+		this.routes(appInit.routes)
+	}
 
-  private async connectDatabase() {
-    let { username, password, host, port, dbName, authName } = this.databaseInfo
-    await new MongoAdapter(username, password, host, port, dbName, authName)
-  }
+	private async connectDatabase() {
+		let { username, password, host, port, dbName, authName } = this.databaseInfo
+		await new MongoAdapter(username, password, host, port, dbName, authName)
+	}
 
-  private pluginsRegister(plugins: { forEach: (arg0: (plugins: any) => void) => void }) {
-    plugins.forEach((plugin) => {
-      this.app.register(plugin)
-    })
-  }
+	private pluginsRegister(plugins: { forEach: (arg0: (plugins: any) => void) => void }) {
+		plugins.forEach((plugin) => {
+			this.app.register(plugin)
+		})
+	}
 
-  public routes(routes: { forEach: (arg0: (routes: any) => void) => void }) {
-    routes.forEach((route) => {
-      let router = new route()
-      this.app.register(router.routes, { prefix: router.prefix_route })
-    })
-  }
+	public routes(routes: { forEach: (arg0: (routes: any) => void) => void }) {
+		routes.forEach((route) => {
+			let router = new route()
+			this.app.register(router.routes, { prefix: router.prefix_route })
+		})
+	}
 
-  public listen() {
-    this.app.listen(this.app_port, () => {
-      console.log(`Jobs Management Service 💰💸 \nlistening on the http://${this.app_doamain}:${this.app_port} 🌟`)
-    })
-  }
+	public listen() {
+		this.app.listen(process.env.PORT || this.app_port, () => {
+			console.log(
+				`Jobs Management Service 💰💸 \nlistening on the http://${this.app_doamain}:${this.app_port} 🌟`,
+			)
+		})
+	}
 }
 
 export default App
