a671c5a2d423915533e754e80bdf184f66d9af21
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-06 15:40:18 +0700

    [+] add : carrier adpater

---
+++ src/adapters/carrier.adapter.ts
@@ -0,0 +1,93 @@
+import axios from 'axios'
+import config from '../config/config'
+import { AccountIdentifier } from '../entities/interfaces/data/account.interface'
+class CarrierAdapter {
+	private prefix = config.carrier.api.prefix_route
+	private prefix_driver = `/driver`
+	private prefix_truck = `/truck`
+	private fetcher = axios.create({
+		baseURL: config.carrier.api.base_url,
+		headers: {
+			common: {
+				Authorization: `bearer ${config.app.token}`,
+			},
+		},
+	})
+
+	/* 
+        CARRIER ADAPTER
+    */
+
+	findAccountByUsername = async (username: string) => {
+		try {
+			const res = await this.fetcher.get(`${this.prefix}/srv/profile/${username}`)
+			return res
+		} catch (error) {
+			return error.response.data
+		}
+	}
+
+	updateJobHistory = async (identifier: AccountIdentifier, job_id: string) => {
+		try {
+			const res = await this.fetcher.put(`${this.prefix}/srv/job/history/add`, { identifier, job_id })
+			return res
+		} catch (error) {
+			return error.response.data
+		}
+	}
+
+	deleteJobHistory = async (identifier: AccountIdentifier, job_id: string) => {
+		try {
+			const res = await this.fetcher.put(`${this.prefix}/srv/job/history/delete`, { identifier, job_id })
+			return res
+		} catch (error) {
+			return error.response.data
+		}
+	}
+
+	/* 
+        DRIVER ADAPTER
+    */
+
+	findDriverByFiltering = async (identifier: AccountIdentifier, query: any) => {
+		try {
+			const res = await this.fetcher.post(`${this.prefix_driver}/srv/filter`, { identifier, query })
+			return res
+		} catch (error) {
+			return error.response.data
+		}
+	}
+
+	updateDriverStatus = async (identifier: AccountIdentifier, driver: any) => {
+		try {
+			const res = await this.fetcher.put(`${this.prefix_driver}/srv/update`, { identifier, ...driver })
+			return res
+		} catch (error) {
+			return error.response.data
+		}
+	}
+
+	/* 
+        TRUCK ADAPTER
+    */
+
+	findTruckByFiltering = async (identifier: AccountIdentifier, query: any) => {
+		try {
+			const res = await this.fetcher.post(`${this.prefix_truck}/srv/filter`, { identifier, query })
+			return res
+		} catch (error) {
+			return error.response.data
+		}
+	}
+
+	updateTruckStatus = async (identifier: AccountIdentifier, truck: any) => {
+		try {
+			const res = await this.fetcher.put(`${this.prefix_truck}/srv/update`, { identifier, ...truck })
+			return res
+		} catch (error) {
+			return error.response.data
+		}
+	}
+}
+
+export default new CarrierAdapter()
