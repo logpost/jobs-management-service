685d2c110ca46588c62179a5805aef23cdf4d317
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-06 15:43:24 +0700

    [+] add : carrier adapter to account factory

--- src/entities/interfaces/data/account.factory.interface.ts
+++ src/entities/interfaces/data/account.factory.interface.ts
@@ -1,7 +1,8 @@
 import ShipperAdapter from '../../../adapters/shipper.adapter'
-
+import CarrierAdapter from '../../../adapters/carrier.adapter'
 interface AccountFactoryInterface {
-    shipper: typeof ShipperAdapter
+	shipper: typeof ShipperAdapter
+	carrier: typeof CarrierAdapter
 }
 
 export default AccountFactoryInterface
