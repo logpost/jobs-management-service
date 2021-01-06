9273a9773e7ed8205d6e5f2116eb1efeb959170d
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-06 15:45:26 +0700

    [+] add : whitelist for update field

--- src/entities/whitelists/whitelist.entity.ts
+++ src/entities/whitelists/whitelist.entity.ts
@@ -1,12 +1,27 @@
 import { Whitelists } from '../interfaces/data/whitelist.interface'
 
 export const whitelist: Whitelists = {
-	jobs: {
-		name: { field: 'name' },
-		display_name: { field: 'display_name' },
-		tel: { field: 'tel' },
-		address: { field: 'address' },
-		account_description: { field: 'account_description' },
-		juristic_id: { field: 'juristic_id' },
+	shipper: {
+		pickup_location: { field: 'pickup_location' },
+		dropoff_location: { field: 'dropoff_location' },
+		pickup_date: { field: 'pickup_date' },
+		dropoff_date: { field: 'dropoff_date' },
+		weight: { field: 'weight' },
+		carrier_specification: { field: 'carrier_specification' },
+		product_type: { field: 'product_type' },
+		offer_price: { field: 'offer_price' },
+		description: { field: 'description' },
+		status: { field: 'status' },
+		distance: { field: 'distance' },
+		permission: { field: 'permission' },
+		waiting_time: { field: 'waiting_time' },
+	},
+	carrier: {
+		driver_id: { field: 'driver_id' },
+		truck_id: { field: 'truck_id' },
+		status: { field: 'status' },
+	},
+	driver: {
+		status: { field: 'status' },
 	},
 }
