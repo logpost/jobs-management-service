cbcbfa17ead0ffc67c7dbaf8717f2de2cfaad10e
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-06 15:41:12 +0700

    [.] indent change

--- src/adapters/shipper.adapter.ts
+++ src/adapters/shipper.adapter.ts
@@ -1,26 +1,26 @@
 import axios from 'axios'
 import config from '../config/config'
+import { AccountIdentifier } from '../entities/interfaces/data/account.interface'
 
 class ShipperAdapter {
+	private prefix = config.shipper.api.prefix_route
+	private fetcher = axios.create({
+		baseURL: config.shipper.api.base_url,
+		headers: {
+			common: {
+				Authorization: `bearer ${config.app.token}`,
+			},
+		},
+	})
 
-    private prefix = config.shipper.api.prefix_route
-    private fetcher = axios.create({
-        baseURL: config.shipper.api.base_url,
-        headers: {
-            common: {
-                'Authorization': `bearer ${config.app.token}`
-            }
-        }
-    });
-
-    updateJobHistory = async (shipper_id: string, job_id: string) => {
-        try {
-            const res = await this.fetcher.put(`${this.prefix}/job/push`, { shipper_id, job_id })
-            return res
-        } catch (error) {
-            return error.response.data
-        }
-    }
+	updateJobHistory = async (identifier: AccountIdentifier, job_id: string) => {
+		try {
+			const res = await this.fetcher.put(`${this.prefix}/srv/job/history/add`, { identifier, job_id })
+			return res
+		} catch (error) {
+			return error.response.data
+		}
+	}
 }
 
 export default new ShipperAdapter()
