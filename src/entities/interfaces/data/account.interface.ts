a9bfa7b1b12992180983919ed11bfdf83cee7136
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-06 15:47:00 +0700

    [+] add : carrier adapter to account factory (2)

---
+++ src/entities/interfaces/data/account.interface.ts
@@ -0,0 +1,7 @@
+interface AccountIdentifier {
+	username?: string
+	shipper_id?: string
+	carrier_id?: string
+}
+
+export { AccountIdentifier }
--- src/factorys/account.factory.ts
+++ src/factorys/account.factory.ts
@@ -1,14 +1,13 @@
 import AccountFactoryInterface from '../entities/interfaces/data/account.factory.interface'
 
 import ShipperAdapter from '../adapters/shipper.adapter'
-// import CarrierAdapter from '../adapters/carrier.adapter'
-
+import CarrierAdapter from '../adapters/carrier.adapter'
 
 class AccountFactory {
-    public account: AccountFactoryInterface  = {
-        shipper: ShipperAdapter,
-        // carrier: CarrierAdapter
-    }
+	public account: AccountFactoryInterface = {
+		shipper: ShipperAdapter,
+		carrier: CarrierAdapter,
+	}
 }
 
 export default AccountFactory
