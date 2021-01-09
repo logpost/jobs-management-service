47f1d40009422b954a3f571ae60f26e8596e56e4
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-09 17:05:29 +0700

    [+] add : status 401 error code on error-handler

--- src/helper/errors.handler.ts
+++ src/helper/errors.handler.ts
@@ -2,29 +2,32 @@ import { ResponseInterface } from '../entities/interfaces/data/response.interfac
 import { FastifyReply } from 'fastify'
 
 const reply = async (replyponseData: ResponseInterface, reply: FastifyReply) => {
+	reply.header('Content-Type', 'application/json;charset=utf-8').code(200)
 
-  reply.header('Content-Type', 'application/json;charset=utf-8').code(200)
-
-  if ('error' in replyponseData) {
-    switch (replyponseData.error.code) {
-      case 400: {
-        reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
-        break
-      }
-      case 403: {
-        reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
-        break
-      }
-      case 404: {
-        reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
-        break
-      }
-      default: {
-        reply.header('Content-Type', 'application/json;charset=utf-8').code(500)
-        break
-      }
-    }
-  }
-  //  return responseCode
+	if ('error' in replyponseData) {
+		switch (replyponseData.error.code) {
+			case 400: {
+				reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
+				break
+			}
+			case 401: {
+				reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
+				break
+			}
+			case 403: {
+				reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
+				break
+			}
+			case 404: {
+				reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
+				break
+			}
+			default: {
+				reply.header('Content-Type', 'application/json;charset=utf-8').code(500)
+				break
+			}
+		}
+	}
+	//  return responseCode
 }
 export default { reply }
