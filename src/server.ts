2eb6a058001bbb1a16566fc42a77fe75f7bea5b6
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-24 19:39:33 +0700

    [+] add : enable Profiler

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
+import * as Profiler from '@google-cloud/profiler'
 import App from './app'
 
 import AuthPlugin from './plugins/auth.plugin'
 import CorsPlugin from './plugins/cors.plugin'
 
 import JobsRoutes from './routes/jobs.route'
+
+if ((process.env.NODE_ENV as string) === 'staging') {
+	Profiler.start({
+		projectId: 'logpost-298506',
+		serviceContext: {
+			service: 'jobs-management-service',
+			version: '1.0.0',
+		},
+	})
+}
 
 const app = new App({
 	routes: [JobsRoutes],
